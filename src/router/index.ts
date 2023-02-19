748404e20559e110d429bf4ff652ea78214f25c2
Gavin <gavin@inncom.cloud>
2023-02-19 18:00:14 +0800

    inti

---
+++ src/router/index.ts
@@ -0,0 +1,23 @@
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
+import Home from '../views/Index.vue';
+
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: '/',
+    component: Home,
+  },
+  {
+    path: '/about',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+});
+
+export default router;
